4a329206f509a35e0f426f7e602893ac0f831d9b
Clayton Galea <claytongalea78@outlook.com>
2026-05-13 09:27:23 +0200

    Initial microservices architecture setup

---
+++ Clayton_CabBookingPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Clayton_CabBookingPlatform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
